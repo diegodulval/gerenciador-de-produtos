9b143b6c77b8281e885c14b4bd4d962209a5e18e
diegodulval <diegodifreitas@gmail.com>
2018-03-30 23:10:20 -0300

    added api config

---
+++ src/Api.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://localhost:3001/'
+})
+const apis = {
+    createProduto: (prod) => api.post('produtos', prod),
+    loadProdutos: (catId) => api.get('/produtos?categoria=' + catId),
+    removeProduto: (id) => api.delete('produtos/' + id),
+    readProduto: (id) => api.get('produtos/'+id),
+    editProduto: (prod) => api.put('produtos/' + prod.id, prod),
+
+    readCategoria: (id) => api.get('categorias/'+id),
+    loadCategorias: () => api.get('categorias'),
+    removeCategoria: (id) => api.delete('categorias/' + id),
+    createCategoria: (cat) => api.post('categorias', cat),
+    editCategoria: (cat) => api.put('categorias/' + cat.id, cat)
+
+}
+
+export default apis
